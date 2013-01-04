2aab4e12b8dad54c7a9b637046f9bd0461b5d501
Ariel <ariel@ariel-laptop.(none)>
2013-01-04 03:23:12 -0800

    Adding Rails app hello_world

---
+++ hello_world/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'f2c395af2172577ffff6158b06e5beeeb7dcd2f1afca1581670b7ae5af3ed5a632d470312395a0ddff2b038f3bcb0bde847280442fa199a0e4f9a67c32ce1238'
